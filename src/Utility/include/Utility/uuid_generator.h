1819c53b3403cbdd860c2b2a9983b87835a8e0f5
Bilal Kahraman <kahramannbilal@gmail.com>
2024-08-15 23:49:16 +0300

    Add uuid external library, create uuid_generator and update utility

---
+++ src/Utility/include/Utility/uuid_generator.h
@@ -0,0 +1,37 @@
+/**
+ * @file uuid_generator.h
+ * @author Bilal Kahraman (kahramannbilal@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef UTILITY_UUID_GENERATOR_H
+#define UTILITY_UUID_GENERATOR_H
+
+#include <third-party/uuid_v4/uuid_v4.h>
+
+namespace wolfenstein {
+
+class UuidGenerator
+{
+  public:
+	static UuidGenerator& GetInstance();
+	UuidGenerator(const UuidGenerator&) = delete;
+	UuidGenerator& operator=(const UuidGenerator&) = delete;
+	~UuidGenerator() = default;
+
+	UUIDv4::UUID GenerateUuid();
+
+  private:
+	UuidGenerator();
+	UUIDv4::UUIDGenerator<std::mt19937_64> generator_;
+	static UuidGenerator* instance_;
+};
+
+}  // namespace wolfenstein
+
+#endif	// UTILITY_UUID_GENERATOR_H
